3809efd91ec02ac4a22097454538a50d5b3c2662
Andrey <ScootalooRainbow@yandex.ru>
2021-01-24 02:02:41 +0900

    check

--- app.js
+++ app.js
@@ -14,19 +14,7 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(helmet());
-app.all("/*", function (req, res, next) {
-
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
-  res.header("Access-Control-Allow-Credentials",true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Accept,X-Access-Token,X-Key,Authorization,X-Requested-With,Origin,Access-Control-Allow-Origin,Access-Control-Allow-Credentials');
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-  } else {
-    next();
-  }
-});
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
