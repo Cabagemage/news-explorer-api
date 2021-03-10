a57dbabaf86450e51b460a681b24cc368061f1b9
Andrey <ScootalooRainbow@yandex.ru>
2021-03-10 15:25:27 +0900

    Fixes

--- app.js
+++ app.js
@@ -1,45 +1,59 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const { Joi, celebrate, errors } = require('celebrate');
-const helmet = require('helmet');
-const { limiter } = require('./utils/optional/limiter.js');
-const router = require('./routes/index.js');
-const { createUser, login } = require('./controllers/users');
-const auth = require('./middlewares/auth');
-const { mongoDBUrl, mongoDBOptions } = require('./utils/configs/config.js');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { Joi, celebrate, errors } = require("celebrate");
+const helmet = require("helmet");
+const { limiter } = require("./utils/optional/limiter.js");
+const router = require("./routes/index.js");
+const { createUser, login } = require("./controllers/users");
+const auth = require("./middlewares/auth");
+const { mongoDBUrl, mongoDBOptions } = require("./utils/configs/config.js");
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 const app = express();
 
 app.use(helmet());
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email().error(new Error('Email обязательное поле!')),
-    password: Joi.string().required().min(3).error(new Error('Пароль должен состоять минимум из 3 символов')),
+app.post(
+  "/signin",
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string()
+        .required()
+        .email()
+        .error(new Error("Email обязательное поле!")),
+      password: Joi.string()
+        .required()
+        .min(3)
+        .error(new Error("Пароль должен состоять минимум из 3 символов")),
+    }),
   }),
-}), login);
+  login
+);
 
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(3),
+app.post(
+  "/signup",
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required().min(2).max(30),
+      email: Joi.string().required().email(),
+      password: Joi.string().required().min(3),
+    }),
   }),
-}), createUser);
+  createUser
+);
 
 app.use(errors());
 app.use(auth);
-app.use('/', router);
+app.use("/", router);
 
-app.get('/crash-test', () => {
+app.get("/crash-test", () => {
   setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
+    throw new Error("Сервер сейчас упадёт");
   }, 0);
 });
 
@@ -54,4 +68,4 @@ app.use((err, req, res, next) => {
   next();
 });
 
-app.listen(PORT, () => console.log('SERVER IS RUNNING'));
+app.listen(PORT, () => console.log("SERVER IS RUNNING"));
